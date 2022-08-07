dd6550b7e403d2319e9edd866b10ad4c3a6b3a03
PedroHenrique-git <pedromf34@outlook.com>
2022-08-07 16:46:48 -0300

    feat: remove function

--- binary-tree/main.c
+++ binary-tree/main.c
@@ -49,6 +49,12 @@ int max(int valueOne, int valueTwo) {
     return valueOne > valueTwo ? valueOne : valueTwo;
 }
 
+Node * minNode(Node * node) {
+    Node * current = node;
+    while(current != NULL && current->left != NULL) current = current->left;
+    return current;
+}
+
 Node * rotationLL(Node * node) {
     Node * tmp = node->left;
     node->left = tmp->right;
@@ -105,10 +111,74 @@ Node * insertNode(Node * node, int element) {
     return node;
 }
 
+Node * removeNode(Node * node, int element) {
+    if(node == NULL) return NULL;
+
+    if(compareValues(element, node->value) == LESS_THAN) {
+        node->left = removeNode(node->left, element);
+        return node;
+    } else if(compareValues(element, node->value) == GREATER_THAN) {
+        node->right = removeNode(node->right, element);
+        return node;
+    } else {
+        if(node->left == NULL && node->right == NULL) {
+            node = NULL;
+            return node;
+        }
+
+        if(node->left == NULL) {
+            node = node->right;
+            return node;
+        } else if(node->right == NULL) {
+            node = node->left;
+            return node;
+        }
+
+        Node * aux = minNode(node->right);
+        node->value = aux->value;
+        node->right = removeNode(node->right, aux->value);
+
+        return node;
+    }
+}
+
 void insert(AvlTree * avlTree, int element) {
     avlTree->root = insertNode(avlTree->root, element);
 }
 
+Node * removeFn(Node * node, int element) {
+    node = removeNode(node, element);
+
+    if(node == NULL) return node;
+
+    int balanceFactor = getBalanceFactor(node);
+
+    if(balanceFactor == UNBALANCED_LEFT) {
+        int balanceFactorLeft = getBalanceFactor(node->left);
+
+        if(balanceFactorLeft == BALANCED || balanceFactorLeft == SLIGHTLY_UNBALANCED_LEFT) {
+            return rotationLL(node);
+        }
+
+        if(balanceFactorLeft == SLIGHTLY_UNBALANCED_RIGHT) {
+            return rotationLR(node->left);
+        }
+    }
+
+    if(balanceFactor == UNBALANCED_RIGHT) {
+        int balanceFactorRight = getBalanceFactor(node);
+
+        if(balanceFactorRight == BALANCED || balanceFactorRight == SLIGHTLY_UNBALANCED_RIGHT) {
+            return rotationRR(node);
+        }
+
+        if(balanceFactorRight == SLIGHTLY_UNBALANCED_LEFT) {
+            return rotationRL(node->right);
+        }
+    }
+
+    return node;
+}
 
 int getNodeHeight(Node * node) {
     if(node == NULL) return -1;
@@ -145,6 +215,12 @@ void menu(int option, AvlTree * avlTree) {
 
             insert(avlTree, valueOfNode);
         break;
+        case 2:
+            printf("\nDigite o valor do no que deseja remover: ");
+            scanf("%d", &valueOfNode);
+
+            removeFn(avlTree->root, valueOfNode);
+        break;
         default:
             printf("\nOpcao invalida!\n");
         break;
@@ -159,7 +235,7 @@ int main()
     AvlTree * avlTree = createTree();
 
     do {
-        printf("0 - Encerrar o programa\n1 - Inserir no na arvore\n\n");
+        printf("0 - Encerrar o programa\n1 - Inserir no na arvore\n2 - Remover no da arvore\n\n");
         scanf("%d", &option);
 
         menu(option, avlTree);
